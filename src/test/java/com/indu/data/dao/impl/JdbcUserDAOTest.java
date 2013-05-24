c4a9eea779c060253aee045a6157829311637f1a
Indudhar <indudhar.devanath@yahoo.com.au>
2013-05-24 17:02:55 +1000

    Testing updates

--- src/test/java/com/indu/data/dao/impl/JdbcUserDAOTest.java
+++ src/test/java/com/indu/data/dao/impl/JdbcUserDAOTest.java
@@ -1,9 +1,18 @@
 package com.indu.data.dao.impl;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.indu.data.User;
+import com.indu.data.dao.UserDAO;
 
 /**
  * Tests for JdbcUserDAO
@@ -14,14 +23,36 @@ import org.junit.runner.RunWith;
 @RunWith(JUnit4ClassRunner.class)
 public class JdbcUserDAOTest {
 	
+	UserDAO userDAO;
+	User mockUser;
+	
+	/**
+	 * Get the Application Context so you can test the jdbcUserDAO bean.
+	 * Note that this test will connect to database as defined in the test resources configuration.
+	 * 
+	 */
 	@Before
 	public void initialize(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		this.userDAO = (UserDAO)context.getBean("jdbcUserDAO");
 		
+		this.mockUser = mock(User.class);
+		when(this.mockUser.getId()).thenReturn(1);
+		when(this.mockUser.getName()).thenReturn("Lisa");
+		when(this.mockUser.getEmail()).thenReturn("lisa@email.com");
 	}
 
 	@Test
+	public void findByUserName(){
+		User testUser = this.userDAO.findByUserName("indu");
+		assertEquals(testUser.getName(), "indu");
+	}
+	
+	@Test
 	public void insertTest(){
-		
+		this.userDAO.insert(this.mockUser);
+		User testUser = this.userDAO.findByUserName(this.mockUser.getName());
+		assertEquals(testUser.getEmail(), this.mockUser.getEmail());
 	}
 	
 	@Test
@@ -38,9 +69,5 @@ public class JdbcUserDAOTest {
 	public void findById(){
 		
 	}
-	
-	@Test
-	public void findByUserName(){
-		
-	}
+
 }
