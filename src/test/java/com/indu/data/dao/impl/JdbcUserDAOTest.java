c467cd79eb4b81932ae98b987330c3af1baa92b1
Indudhar <indudhar.devanath@yahoo.com.au>
2013-05-24 15:15:50 +1000

    Adding unit test class

---
+++ src/test/java/com/indu/data/dao/impl/JdbcUserDAOTest.java
@@ -0,0 +1,46 @@
+package com.indu.data.dao.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for JdbcUserDAO
+ * 
+ * @author Indu Devanath
+ *
+ */
+@RunWith(JUnit4ClassRunner.class)
+public class JdbcUserDAOTest {
+	
+	@Before
+	public void initialize(){
+		
+	}
+
+	@Test
+	public void insertTest(){
+		
+	}
+	
+	@Test
+	public void updateTest(){
+		
+	}
+	
+	@Test
+	public void deleteByIdTest(){
+		
+	}
+	
+	@Test
+	public void findById(){
+		
+	}
+	
+	@Test
+	public void findByUserName(){
+		
+	}
+}
