349889ec7b1a14bf58bd229ccb06688567f63c9b
Indudhar <indudhar.devanath@yahoo.com.au>
2013-05-24 17:04:29 +1000

    Controller cleanup

--- src/main/java/com/indu/controller/OrganizerController.java
+++ src/main/java/com/indu/controller/OrganizerController.java
@@ -1,14 +1,16 @@
 package com.indu.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.indu.data.User;
-import com.indu.data.dao.UserDAO;
+/**
+ * Organizer Controller to recieve and service http requests
+ * 
+ * @author Indu Devanath
+ *
+ */
 
 @Controller
 @RequestMapping("/")
@@ -16,18 +18,9 @@ public class OrganizerController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		testingJDBC();
 		model.addAttribute("message", "Starter App");
 		return "start";
 
-	}
-	
-	public boolean testingJDBC(){
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		UserDAO userDAO = (UserDAO)context.getBean("jdbcUserDAO");
-		User user = userDAO.findById(1);
-		System.out.println(user.getEmail());
-		return true;
 	}
 	
 }
