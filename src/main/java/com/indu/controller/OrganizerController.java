9f0ca6eae2bb841645c27c653599c515b2d1cd4f
Indudhar <indudhar.devanath@yahoo.com.au>
2013-06-03 20:47:31 +1000

    add static resources to add style sheets, java scripts and others

--- src/main/java/com/indu/controller/OrganizerController.java
+++ src/main/java/com/indu/controller/OrganizerController.java
@@ -3,7 +3,6 @@ package com.indu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Organizer Controller to recieve and service http requests
@@ -13,10 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 
 @Controller
-@RequestMapping("/")
 public class OrganizerController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping({"/","/home","/start"})
 	public String printWelcome(ModelMap model) {
 		
 		model.addAttribute("message", "Starter App");
