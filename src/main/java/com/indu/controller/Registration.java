0137b8273ad778946e4fccd03bb9127248347366
Indudhar <indudhar.devanath@yahoo.com.au>
2013-06-10 10:06:33 +1000

    add datasource and dao configuration

--- src/main/java/com/indu/controller/Registration.java
+++ src/main/java/com/indu/controller/Registration.java
@@ -2,6 +2,7 @@ package com.indu.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.indu.controller.forms.Registrant;
+import com.indu.data.dao.UsersDAO;
 
 /**
  * @author Indu Devanath
@@ -17,6 +19,9 @@ import com.indu.controller.forms.Registrant;
 @Controller
 @RequestMapping("/new_registration")
 public class Registration {
+	
+	@Autowired
+	private UsersDAO usersDAO;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String createRegForm(Model model){
