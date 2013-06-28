93b219cea7d3cb5f9fd5f67f86cc26b3524dc491
Indudhar <indudhar.devanath@yahoo.com.au>
2013-06-29 09:34:43 +1000

    change table task and task_detail

--- sql-scripts/tasks.sql
+++ sql-scripts/tasks.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE task(
 	id SERIAL,
+	detail_id INTEGER,
 	task_title VARCHAR(20) NOT NULL,
-	CONSTRAINT task_pk PRIMARY KEY (id)
+	CONSTRAINT task_pk PRIMARY KEY (id),
+	CONSTRAINT task_fk FOREIGN KEY (detail_id) REFERENCES task_detail(id) ON DELETE CASCADE
 );
 
 CREATE TABLE task_detail(
 	id SERIAL,
-	task_id INTEGER,
 	task_description VARCHAR(400),
 	creation_date DATE NOT NULL,
 	due_date DATE NOT NULL,
 	CONSTRAINT task_detail_pk PRIMARY KEY (id),
-	CONSTRAINT task_detail_fk FOREIGN KEY (task_id) REFERENCES task(id) ON DELETE CASCADE
 );
